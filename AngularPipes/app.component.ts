8690c4feb1a8e31d35926e5430bae68bc4f46384
HarshaVardhan <athaluri.harsha@gmail.com>
2023-03-07 08:54:34 +0530

    Pipe Demo

---
+++ AngularPipes/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+  price = 100;
+
+  msg = 'hello';
+
+  temp = 13;
+}
